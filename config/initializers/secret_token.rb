223ae219e88445f58c663ee4db7864987fe3d35e
Igor Zubkov <igor.zubkov@gmail.com>
2012-02-25 17:51:45 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reader::Application.config.secret_token = 'cc37161491c3a82a6f842a7cd6a4ea8b2db7843c3f0154336e19531f3d8c5a21a78159028ab9b9b8ed26b39787aa7708e859f90099af23eb6340d1b2c8eb147e'
