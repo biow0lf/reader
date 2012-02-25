bdbcfc99165e20cec53abc597e18b3c56e2c8d67
Igor Zubkov <igor.zubkov@gmail.com>
2012-02-25 19:07:59 +0200

    FeedItems updates

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -4,4 +4,6 @@ class FeedItem < ActiveRecord::Base
   belongs_to :feed
 
   validates :feed, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- spec/models/feed_item_spec.rb
+++ spec/models/feed_item_spec.rb
@@ -6,6 +6,8 @@ describe FeedItem do
   it { should belong_to :feed }
 
   it { should validate_presence_of :feed }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 
   it { should have_db_index :feed_id }
 end
