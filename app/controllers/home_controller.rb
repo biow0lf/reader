16a408cc48f5f8fa0db5e1510362c03f6a8d02d5
Igor Zubkov <igor.zubkov@gmail.com>
2012-02-25 21:33:56 +0200

    Redirect to dashboard if user is logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  before_filter :redirect_if_logged_in
+
   def index
+  end
+
+private
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
   end
 end
