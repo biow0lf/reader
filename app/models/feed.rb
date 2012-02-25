33ac98c1c6a8b064cd2e298b14f528ccc04a6958
Igor Zubkov <igor.zubkov@gmail.com>
2012-02-25 19:33:22 +0200

    Validate link and title for Feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,5 +3,10 @@
 class Feed < ActiveRecord::Base
   has_many :feed_items
 
+  # url of rss feed
   validates :url, presence: true
+  # rss.channel.link
+  validates :link, presence: true
+  # rss.channel.title
+  validates :title, presence: true
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -6,4 +6,6 @@ describe Feed do
   it { should have_many :feed_items }
 
   it { should validate_presence_of :url }
+  it { should validate_presence_of :link }
+  it { should validate_presence_of :title }
 end
